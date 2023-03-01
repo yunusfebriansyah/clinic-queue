4d2dac0c2030abdf4215bcdbcbef457ff2b02e50
yunusfebriansyah <febriansyahyunus369@gmail.com>
2023-03-01 14:08:17 +0700

    Doctor : Fixing Index Dashboard

--- app/Http/Controllers/DoctorDashboardController.php
+++ app/Http/Controllers/DoctorDashboardController.php
@@ -15,7 +15,7 @@ class DoctorDashboardController extends Controller
     {
         return view('doctor.layouts.index', [
             'title' => 'Dashboard Dokter',
-            'is_done_treatments' => count(Treatment::where('doctor_id', auth()->user()->id)->where('status', 'selesai')->get()),
+            'is_done_treatments' => count(Treatment::where('doctor_id', auth()->user()->id)->where('status', '!=', 'ditolak')->where('status', '!=', 'dibatalkan')->where('status', '!=', 'menunggu konfirmasi')->where('status', '!=', 'menunggu antrian')->where('status', '!=', 'ditangani')->get()),
             'is_queue_treatments_today' => count(Treatment::where('doctor_id', auth()->user()->id)->where('status', '!=', 'ditolak')->where('status', '!=', 'dibatalkan')->where('status', '!=', 'menunggu konfirmasi')->where('created_at', '>=', Carbon::today())->get()),
             'is_queue_treatments' => count(Treatment::where('doctor_id', auth()->user()->id)->where('status', 'menunggu antrian')->where('created_at', '>=', Carbon::today())->get())
         ]);
